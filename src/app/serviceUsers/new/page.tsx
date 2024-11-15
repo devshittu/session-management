cb12b4fbcd7b800d489de100dcbd1c309328a286
Muhammed Shittu <devshittu@gmail.com>
2024-11-15 22:40:49 +0000

    patients to serviceUsers renaming

---
+++ src/app/serviceUsers/new/page.tsx
@@ -0,0 +1,12 @@
+// app/serviceUsers/new/page.tsx
+
+import NewServiceUserForm from './NewServiceUserForm';
+
+export default function NewServiceUserPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Add New ServiceUser</h1>
+      <NewServiceUserForm />
+    </div>
+  );
+}
