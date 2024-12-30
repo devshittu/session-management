43298e64ea32a8f32479b150a2764b134aa2f7df
Muhammed Shittu <devshittu@gmail.com>
2024-12-30 08:35:06 +0000

    add looks and feel component

---
+++ src/components/Buttons/Button.tsx
@@ -0,0 +1,32 @@
+// src/components/button.tsx
+'use client';
+
+import React, { ButtonHTMLAttributes } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: 'primary' | 'secondary' | 'danger';
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  variant = 'primary',
+  className,
+  ...props
+}) => {
+  const baseClasses = 'px-4 py-2 rounded text-sm font-medium focus:outline-none';
+  const variantClasses = {
+    primary: 'bg-blue-500 text-white hover:bg-blue-600',
+    secondary: 'bg-gray-200 text-black hover:bg-gray-300',
+    danger: 'bg-red-500 text-white hover:bg-red-600',
+  };
+
+  return (
+    <button
+      className={twMerge(baseClasses, variantClasses[variant], className)}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
