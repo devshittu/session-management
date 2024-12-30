d5a6a90709a30d47fddfc189e250c29ecac80013
Muhammed Shittu <devshittu@gmail.com>
2024-12-30 08:40:38 +0000

    revamp: update

--- src/app/serviceUsers/page.tsx
+++ src/app/serviceUsers/page.tsx
@@ -1,43 +1,4 @@
-// // app/serviceUsers/page.tsx
-// import { prisma } from '@/lib/prisma';
-
 import ServiceUsersList from './ServiceUsersList';
-
-// interface ServiceUser {
-//   id: number;
-//   name: string;
-//   ward: { name: string };
-// }
-
-// export default async function ServiceUsersPage() {
-//   const serviceUsers: ServiceUser[] = await prisma.serviceUser.findMany({
-//     include: { ward: true },
-//   });
-
-//   return (
-//     <div>
-//       <h1>ServiceUsers</h1>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>Name</th>
-//             <th>Ward</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {serviceUsers.map((serviceUser) => (
-//             <tr key={serviceUser.id}>
-//               <td>{serviceUser.name}</td>
-//               <td>{serviceUser.ward.name}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
-
-// app/serviceUsers/page.tsx
 
 export default function ServiceUsersPage() {
   return (
@@ -47,3 +8,4 @@ export default function ServiceUsersPage() {
     </div>
   );
 }
+// src/app/serviceUsers/page.tsx
