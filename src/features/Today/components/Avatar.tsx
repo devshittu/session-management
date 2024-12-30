ce0ce4711dea704b93a5f7b5e388da56afff4cb4
Muhammed Shittu <devshittu@gmail.com>
2024-12-30 08:31:27 +0000

    add looks and feel

---
+++ src/features/Today/components/Avatar.tsx
@@ -0,0 +1,25 @@
+// src/components/Avatar.tsx
+import React from 'react';
+
+type AvatarProps = {
+  src: string;
+  size?: 'sm' | 'md' | 'lg';
+};
+
+export const Avatar: React.FC<AvatarProps> = ({ src, size = 'md' }) => {
+  const sizes = {
+    sm: 'h-8 w-8',
+    md: 'h-12 w-12',
+    lg: 'h-16 w-16',
+  };
+
+  return (
+    <img
+      className={`relative rounded-full border-2 border-white saturate-0 ${sizes[size]}`}
+      src={src}
+      width="256"
+      height="256"
+      alt="Avatar"
+    />
+  );
+};
