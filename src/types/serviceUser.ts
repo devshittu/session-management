972a45e465ebad1be16a78b3240355bcf86ae755
Muhammed Shittu <devshittu@gmail.com>
2024-12-30 08:41:53 +0000

    revamp: update

--- src/types/serviceUser.ts
+++ src/types/serviceUser.ts
@@ -1,24 +1,39 @@
-// types/serviceUser.ts
 export type ServiceUserStatus = 'admitted' | 'discharged';
 
-export type ServiceUser = {
+export type Admission = {
   id: number;
-  name: string;
+  serviceUserId: number;
+  wardId: number;
+  admissionDate: string;
+  dischargeDate?: string | null;
   ward: {
     id: number;
     name: string;
   };
-  wardId: number;
-  sessions: Session[];
-  status: ServiceUserStatus;
-  createdAt: string;
-  updatedAt?: string | null;
 };
 
 export type Session = {
   id: number;
-  serviceUserId: number;
+  admissionId: number;
   activityId: number;
   timeIn: string;
   timeOut?: string | null;
+  activity: {
+    id: number;
+    name: string;
+  };
 };
+
+export type ServiceUser = {
+  id: number;
+  nhsNumber: string; // Unique NHS-like identification
+  name: string;
+  // status: ServiceUserStatus;
+  createdAt: string;
+  updatedAt?: string | null;
+  admissions: Admission[]; // A service user can have multiple admissions
+};
+
+
+// src/types/serviceUser.ts
+
