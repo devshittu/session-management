e656d6c6f8c7b93180497466df651319f8fc5582
Muhammed Shittu <devshittu@gmail.com>
2024-12-30 08:36:17 +0000

    add looks and feel component

---
+++ src/components/Icons/MoreIcon.tsx
@@ -0,0 +1,11 @@
+// src/components/Icons/MoreIcon.tsx
+import React from 'react';
+import { BaseIcon } from './BaseIcon';
+
+export const MoreIcon: React.FC = () => (
+  <BaseIcon size={24} viewBox="0 0 24 24">
+    <circle cx="12" cy="12" r="1"></circle>
+    <circle cx="19" cy="12" r="1"></circle>
+    <circle cx="5" cy="12" r="1"></circle>
+  </BaseIcon>
+);
