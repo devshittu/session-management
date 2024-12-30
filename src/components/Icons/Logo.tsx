f2c377e107be549e35f3eb7d987b344ac9ff96e9
Muhammed Shittu <devshittu@gmail.com>
2024-12-30 08:36:56 +0000

    add looks and feel component

---
+++ src/components/Icons/Logo.tsx
@@ -0,0 +1,14 @@
+// src/components/Icons/Logo.tsx
+import React from 'react';
+
+export const Logo: React.FC = () => (
+  <svg
+    className="flex-shrink-0"
+    xmlns="http://www.w3.org/2000/svg"
+    width="200"
+    height="80"
+    viewBox="0 0 500 200"
+  >
+    {/* SVG paths */}
+  </svg>
+);
