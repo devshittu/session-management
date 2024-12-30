e1878c76a131a85da09c7e8035f0efd40db8196d
Muhammed Shittu <devshittu@gmail.com>
2024-12-30 08:40:24 +0000

    revamp: update

---
+++ src/app/activities/new/page.tsx
@@ -0,0 +1,10 @@
+import CreateActivityForm from './CreateActivityForm';
+
+export default function NewActivityPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Create New Activity</h1>
+      <CreateActivityForm />
+    </div>
+  );
+}
