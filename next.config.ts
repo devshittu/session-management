d030a09e7e9143d5688ce12693ecac9535d56b85
Muhammed Shittu <devshittu@gmail.com>
2025-02-01 15:30:01 +0000

    feat: add remote image patterns for external sources in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.generated.photos',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn3d.iconscout.com',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
