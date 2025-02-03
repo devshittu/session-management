6fb6167c694db6df082e1c10e657df01181634a4
Muhammed Shittu <devshittu@gmail.com>
2025-02-03 22:38:05 +0000

    feat: update Header component to use new logo and improve site branding

--- src/components/Blocks/Header.tsx
+++ src/components/Blocks/Header.tsx
@@ -4,6 +4,9 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import ThemeToggle from './ThemeToggle';
+import Image from 'next/image';
+import Logo from '../Illustration/Logo';
+import GradientLogo from '../Illustration/GradientLogo';
 
 const Header: React.FC = () => {
   const [top, setTop] = useState<boolean>(true);
@@ -25,66 +28,19 @@ const Header: React.FC = () => {
         <div className="flex items-center justify-between h-16 md:h-20">
           {/* Site Branding */}
           <div className="flex-shrink-0 mr-4">
-            <Link href="/" aria-label="Cruip">
-              <svg
-                className="w-8 h-8"
-                viewBox="0 0 32 32"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <defs>
-                  <radialGradient
-                    id="header-logo"
-                    cx="21.152%"
-                    cy="86.063%"
-                    fx="21.152%"
-                    fy="86.063%"
-                    r="79.941%"
-                  >
-                    <stop stopColor="#4FD1C5" offset="0%" />
-                    <stop stopColor="#81E6D9" offset="25.871%" />
-                    <stop stopColor="#338CF5" offset="100%" />
-                  </radialGradient>
-                </defs>
-                <rect
-                  width="32"
-                  height="32"
-                  rx="16"
-                  fill="url(#header-logo)"
-                  fillRule="nonzero"
-                />
-              </svg>
+            <Link href="/" aria-label="Activity Manager">
+              <div className="w-24 sm:w-32 md:w-40 lg:w-40 xl:w-40 2xl:w-40 h-auto">
+                <Logo className="text-base-content" />
+              </div>
             </Link>
+            {/* <div className="w-24 sm:w-32 md:w-40 lg:w-48 h-auto">
+  <GradientLogo />
+</div> */}
           </div>
 
           {/* Site Navigation */}
           <nav className="flex flex-grow">
             <ul className="flex flex-grow justify-end flex-wrap items-center">
-              {/* <li>
-                <Link
-                  href="/signin"
-                  className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
-                >
-                  Sign in
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/signup"
-                  className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3 flex items-center"
-                >
-                  <span>Sign up</span>
-                  <svg
-                    className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1"
-                    viewBox="0 0 12 12"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                      fillRule="nonzero"
-                    />
-                  </svg>
-                </Link>
-              </li> */}
               <li>
                 <ThemeToggle />
               </li>
