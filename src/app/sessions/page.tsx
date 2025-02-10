d37f20c429b2337cc2430f8b5d4ed364a22e3405
Muhammed Shittu <devshittu@gmail.com>
2025-02-10 00:14:59 +0000

    feat(sessions): refactor SessionsPage to use DashboardPageFrame with action button for creating new sessions

--- src/app/sessions/page.tsx
+++ src/app/sessions/page.tsx
@@ -1,45 +1,19 @@
-import { prisma } from '@/lib/prisma';
 import InfiniteSessionsList from '../../features/Sessions/ui/InfiniteSessionsList';
-
-type Session = {
-  id: number;
-  admission: {
-    serviceUser: { name: string };
-  };
-  activity: { name: string };
-  timeIn: string;
-  timeOut?: string | null;
-};
+import DashboardPageFrame from '@/components/Frame/DashboardPageFrame';
+import Link from 'next/link';
 
 export default async function SessionsPage() {
-  const sessions = await prisma.session.findMany({
-    include: {
-      admission: {
-        include: { serviceUser: true },
-      },
-      activity: true,
-    },
-  });
-
-  const serializedSessions: Session[] = sessions.map((session) => ({
-    id: session.id,
-    admission: { serviceUser: { name: session.admission.serviceUser.name } },
-    activity: { name: session.activity.name },
-    timeIn: session.timeIn.toISOString(),
-    timeOut: session.timeOut ? session.timeOut.toISOString() : null,
-  }));
-
   return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Sessions</h1>
-      <a
-        href="/sessions/new"
-        className="mb-4 inline-block text-blue-500 hover:underline"
-      >
-        Start New Session
-      </a>
+    <DashboardPageFrame
+      title="Sessions"
+      pageActions={
+        <Link href={`/sessions/new`} className="btn btn-primary">
+          Create New
+        </Link>
+      }
+    >
       <InfiniteSessionsList />
-    </div>
+    </DashboardPageFrame>
   );
 }
 
